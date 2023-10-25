c1950bc245a5e1140d18c638b4862a1210ff11fb
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2023-10-25 10:54:45 +0300

    homework9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+let isChangingTheme = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+startBtn.addEventListener("click", () => {
+    if (isChangingTheme) {
+        return; 
+      }
+      isChangingTheme = true;
+      startBtn.disabled = true;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  if (isChangingTheme) {
+    clearInterval(timerId);
+    isChangingTheme = false;
+    startBtn.disabled = false;
+  console.log(`Interval with id ${timerId} has stopped!`);
+}
+});

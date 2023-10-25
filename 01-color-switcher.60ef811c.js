84294733cd67ba93bddb0fb02bac6b86200031b7
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2023-10-25 08:51:02 +0000

    Deploying to gh-pages from @ HannaKaniuk/goit-js-hw-09@b24f6b1a52afa3a9fde7254d8199e8f04524bc23 🚀

---
+++ 01-color-switcher.60ef811c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=null,a=!1;t.addEventListener("click",(()=>{a||(a=!0,t.disabled=!0,o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),e.addEventListener("click",(()=>{a&&(clearInterval(o),a=!1,t.disabled=!1,console.log(`Interval with id ${o} has stopped!`))})),console.log();
+//# sourceMappingURL=01-color-switcher.60ef811c.js.map

ab37526385fdcf9183d82c304df80cbd51da78f7
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2023-10-27 22:43:20 +0300

    homework9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); 
+
+  const delay = parseInt(form.elements.delay.value, 10);
+  const step = parseInt(form.elements.step.value, 10);
+  const amount = parseInt(form.elements.amount.value, 10);
+
+  handlePromises(amount, step, delay);
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+       reject({position, delay});
+      }
+    }, delay);
+  });
 }
+
+function handlePromises(amount, step){
+  let currentDelay = step;
+
+  for (let i=1; i<=amount; i++){
+    createPromise(i, currentDelay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  currentDelay += step;
+}
+  }
+
